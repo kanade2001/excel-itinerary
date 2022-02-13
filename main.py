80f1c12fe5ff49773259f1f6683854a2e0be5e5e
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-13 16:29:48 +0900

    sample1

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+import openpyxl
+import os
+
+#path = r"C:\Users\Yuki\GoogleDrive_kanade2001.cpp\travel\\"
+
+name = "sample1.xlsx"
+
+
+
+#Make file
+
+filename = name
+#filename = path + filename
+#filename = os.path.expanduser(filename)
+if os.path.exists(filename):
+    print("File exists.")
+    workbook = openpyxl.load_workbook(filename)
+else:
+    print("File doesn't exist.")
+    workbook = openpyxl.Workbook()
+    workbook.save(filename)
+
+worksheet = workbook.create_sheet(title="toppage", index = 0)
+worksheet.cell(row = 1, column = 1, value="this should be the first sheet")
+
+#Data input
+
+
+workbook.save(filename)
