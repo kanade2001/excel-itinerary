a2249341c35087b9b9129cb389d5fdb58f97b969
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-14 22:34:48 +0900

    settings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import openpyxl
 import os
+import csv
 
 #path = r"C:\Users\Yuki\GoogleDrive_kanade2001.cpp\travel\\"
 
@@ -13,17 +14,35 @@ filename = name
 #filename = path + filename
 #filename = os.path.expanduser(filename)
 if os.path.exists(filename):
+    #open file
     print("File exists.")
     workbook = openpyxl.load_workbook(filename)
+    
+    worksheet = workbook.active
+    
 else:
+    #create new file
     print("File doesn't exist.")
     workbook = openpyxl.Workbook()
     workbook.save(filename)
-
-worksheet = workbook.create_sheet(title="toppage", index = 0)
-worksheet.cell(row = 1, column = 1, value="this should be the first sheet")
-
-#Data input
+    
+    #margines settings
+    worksheet = workbook.active
+    worksheet.page_margins.left = 1
+    worksheet.page_margins.right = 1
+    worksheet.page_margins.top = 1
+    worksheet.page_margins.bottom = 1
+    worksheet.page_margins.header = 0
+    worksheet.page_margins.footer = 0
+    
+    #dimensions settings
+    worksheet.column_dimensions['A'].width = 5
+    worksheet.column_dimensions['B'].width = 1
+    worksheet.column_dimensions['C'].width= 5
+    worksheet.column_dimensions['D'].width = 35
+    worksheet.column_dimensions['E'].width = 15
+    worksheet.column_dimensions['F'].width = 10
+    
 
 
 workbook.save(filename)
