ceee28d0f3eddc36b0980512f9d4153399be5d32
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-15 00:11:59 +0900

    day2

--- main.py
+++ main.py
@@ -2,29 +2,37 @@ import openpyxl
 import os
 import csv
 
+import editxlfile
+
 #path = r"C:\Users\Yuki\GoogleDrive_kanade2001.cpp\travel\\"
 
 name = "Sample1"
 
-
-
-#Make file
-
+#
 xlsx_filename = name + ".xlsx"
 #filename = path + filename
 #filename = os.path.expanduser(filename)
+csv_filename = name + ".csv"
+
+
+workbook = ""
+worksheet = ""
+
+
+
 if os.path.exists(xlsx_filename):
     #open file
     print("File exists.")
     workbook = openpyxl.load_workbook(xlsx_filename)
-    
-    worksheet = workbook.active
     
 else:
     #create new file
     print("File doesn't exist.")
     workbook = openpyxl.Workbook()
     workbook.save(xlsx_filename)
+    
+    #edit sheet name
+    workbook.worksheets[0].title = "TITLE"
     
     #margines settings
     worksheet = workbook.active
@@ -44,13 +52,33 @@ else:
     worksheet.column_dimensions['F'].width = 10
     
 
-csv_filename = name + ".csv"
 with open(csv_filename, encoding='utf8',newline="") as csv_file:
     csvreader = csv.reader(csv_file)
     for row in csvreader:
         print(row)
-
-
+        if row[0] == "0":
+            print("Check DATE")
+            worksheet_list = workbook.sheetnames
+            #for ws in worksheet_list:
+            if row[1] in workbook.sheetnames:
+                print()
+            else:
+                workbook.copy_worksheet(workbook.worksheets[0])
+                workbook.worksheets[-1].title = row[1]
+            worksheet = workbook[row[1]]
+        
+        if row[0] == "1":
+            row_write = worksheet.max_row + 1
+            worksheet.cell(row=row_write,column=3).value = row[1]
+            worksheet.cell(row=row_write,column=1).value = row[2]
+            worksheet.cell(row=row_write,column=6).value = row[3] + "番線発"
+            worksheet.cell(row=row_write+3,column=3).value = row[4]
+            worksheet.cell(row=row_write+3,column=1).value = row[5]
+            worksheet.cell(row=row_write+3,column=6).value = row[6] + "番線着"
+            worksheet.cell(row=row_write+1,column=4).value = row[7]
+            worksheet.cell(row=row_write+1,column=5).value = row[8]
+            worksheet.cell(row=row_write+2,column=4).value = row[9]
+            worksheet.cell(row=row_write+2,column=5).value = row[10]
 
 
 workbook.save(xlsx_filename)
