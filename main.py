b56613f05ea363783164450a16724224ac81764c
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-15 00:42:43 +0900

    Day2

--- main.py
+++ main.py
@@ -46,10 +46,10 @@ else:
     #dimensions settings
     worksheet.column_dimensions['A'].width = 5
     worksheet.column_dimensions['B'].width = 1
-    worksheet.column_dimensions['C'].width= 5
-    worksheet.column_dimensions['D'].width = 35
-    worksheet.column_dimensions['E'].width = 15
-    worksheet.column_dimensions['F'].width = 10
+    worksheet.column_dimensions['C'].width= 3
+    worksheet.column_dimensions['D'].width = 7
+    worksheet.column_dimensions['E'].width = 35
+    worksheet.column_dimensions['F'].width = 15
     
 
 with open(csv_filename, encoding='utf8',newline="") as csv_file:
@@ -58,27 +58,23 @@ with open(csv_filename, encoding='utf8',newline="") as csv_file:
         print(row)
         if row[0] == "0":
             print("Check DATE")
-            worksheet_list = workbook.sheetnames
-            #for ws in worksheet_list:
-            if row[1] in workbook.sheetnames:
-                print()
-            else:
+            if row[1] not in workbook.sheetnames:
                 workbook.copy_worksheet(workbook.worksheets[0])
                 workbook.worksheets[-1].title = row[1]
             worksheet = workbook[row[1]]
         
         if row[0] == "1":
             row_write = worksheet.max_row + 1
-            worksheet.cell(row=row_write,column=3).value = row[1]
+            worksheet.cell(row=row_write,column=4).value = row[1]
             worksheet.cell(row=row_write,column=1).value = row[2]
-            worksheet.cell(row=row_write,column=6).value = row[3] + "番線発"
-            worksheet.cell(row=row_write+3,column=3).value = row[4]
+            worksheet.cell(row=row_write,column=3).value = row[3]
+            worksheet.cell(row=row_write+3,column=4).value = row[4]
             worksheet.cell(row=row_write+3,column=1).value = row[5]
-            worksheet.cell(row=row_write+3,column=6).value = row[6] + "番線着"
-            worksheet.cell(row=row_write+1,column=4).value = row[7]
-            worksheet.cell(row=row_write+1,column=5).value = row[8]
-            worksheet.cell(row=row_write+2,column=4).value = row[9]
-            worksheet.cell(row=row_write+2,column=5).value = row[10]
+            worksheet.cell(row=row_write+3,column=3).value = row[6]
+            worksheet.cell(row=row_write+1,column=5).value = row[7]
+            worksheet.cell(row=row_write+1,column=6).value = row[8]
+            worksheet.cell(row=row_write+2,column=5).value = row[9]
+            worksheet.cell(row=row_write+2,column=6).value = row[10]
 
 
 workbook.save(xlsx_filename)
