7ce292fc0e46a673de7fd642c220151576b6c935
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-16 10:11:07 +0900

    create_csv一応完成

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ from config import *
 
 
 workbook = Edit_xlsx.openXLSX(xlsx_filename)
+worksheet = workbook.worksheets[0]
 
 with open(csv_filename, encoding='utf8',newline="") as csv_file:
     csvreader = csv.reader(csv_file)
     for row in csvreader:
-        if row[0] == "DATE":
+        print (row)
+
+        if row[0] == 'DATE':
             worksheet = Edit_xlsx.sheet_edit(workbook,row[1])
         else:
             Edit_xlsx.edit(row,worksheet)
