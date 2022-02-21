f87694f5f262318f7d9779a5a87e7e3d2162df75
kanade2001 <64743279+kanade2001@users.noreply.github.com>
2022-02-21 09:48:49 +0900

    tV2.1.3 コード簡略化

--- GUI_EDIT.py
+++ GUI_EDIT.py
@@ -3,6 +3,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter import messagebox
 from unicodedata import numeric
+from xmlrpc.client import Transport
 
 import config
 import Create_csv
@@ -13,6 +14,7 @@ from GUI_TEMPLATE import *
 
 def main_edit(widget):
     edit_mode = 1
+    MODE_list = ['Auto','Manual']
     Transport_list = ['鉄道','バス','空路','フェリー','徒歩','観光']
 
 #-----------------------------------Header_frame---------------------------
@@ -31,55 +33,58 @@ def main_edit(widget):
     frame = ttk.Frame(widget,height=60)
     frame.pack(expand=1,fill=tk.BOTH)
     
-    #first_frame(Fixed)
-    first_frame = ttk.Frame(frame,height=10)
-    first_frame.pack(fill=tk.X)
-    def Change_Button(num):
-        global edit_mode
-        button_auto.config(state=tk.ACTIVE)
-        button_manual.config(state=tk.ACTIVE)
-        if num==1:
-            button_auto.config(state=tk.DISABLED)
-            second_frame_auto.tkraise()
-            third_frame_auto.tkraise()
-            edit_mode = 1
-        elif num==2:
-            button_manual.config(state=tk.DISABLED)
-            second_frame_manual.tkraise()
-            third_frame_manual.tkraise()
-            edit_mode = 2
-    button_auto = ttk.Button(first_frame,text='Auto',width=10)
-    button_manual = ttk.Button(first_frame,text='Manual',width=10)
-    button_auto.config(command=lambda: Change_Button(1))
-    button_manual.config(command=lambda: Change_Button(2))
-    button_auto.pack(side='left',expand=1,fill='x')
-    button_manual.pack(side='left',expand=1,fill='x')
-    
+    #select_frame(Fixed)
+    select_frame = ttk.Frame(frame,height=10)
+    select_frame.pack(fill=tk.X)
+    #def Change_Button(num):
+    #    global edit_mode
+    #    button_auto.config(state=tk.ACTIVE)
+    #    button_manual.config(state=tk.ACTIVE)
+    #    if num==1:
+    #        button_auto.config(state=tk.DISABLED)
+    #        second_frame_auto.tkraise()
+    #        third_frame_auto.tkraise()
+    #        edit_mode = 1
+    #    elif num==2:
+    #        button_manual.config(state=tk.DISABLED)
+    #        second_frame_manual.tkraise()
+    #        third_frame_manual.tkraise()
+    #        edit_mode = 2
+    #button_auto = ttk.Button(select_frame,text='Auto',width=10)
+    #button_manual = ttk.Button(select_frame,text='Manual',width=10)
+    #button_auto.config(command=lambda: Change_Button(1))
+    #button_manual.config(command=lambda: Change_Button(2))
+    #button_auto.pack(side='left',expand=1,fill='x')
+    #button_manual.pack(side='left',expand=1,fill='x')
     
     #second_frame
     second_frame = ttk.Frame(frame,height=10)
     second_frame.pack(fill=tk.X)
     second_frame.grid_rowconfigure(0, weight=1)
     second_frame.grid_columnconfigure(0, weight=1)
-    
     second_frame_auto = ttk.Frame(second_frame)
-    second_frame_auto.grid(row=0, column=0, sticky='nsew')
-    widget_template.reference_file(second_frame_auto,'Sample1')     #ファイル参照ウィンドウ
-    
+    second_frame_auto.grid(row=0, column=0,sticky='nsew')
     second_frame_manual = ttk.Frame(second_frame)
-    second_frame_manual.grid(row=0, column=0, sticky='nsew')
-    Transport_Buttons = []
-    def Transport_change(num):
-        def Transport_change_main():
-            print('Change to ' + str(num))
-            for i,name in enumerate(Transport_list):
-                Transport_Buttons[i] = tk.ACTIVE
-            Transport_Buttons[num]=tk.DISABLED
-            third_frame_manual_frames[num].tkraise()
-        return Transport_change_main
-    for i,name in enumerate(Transport_list):
-        Transport_Buttons.append(ttk.Button(second_frame_manual,text=name,command=Transport_change(i)))
-        Transport_Buttons[i].pack(side='left',expand=1,fill='both')
+    second_frame_manual.grid(row=0, column=0,sticky='nsew')
+    
+    
+    
+
+    widget_template.reference_file(second_frame_auto,'Sample1')     #ファイル参照ウィンドウ
+    #Transport_Buttons = []
+    #def Transport_change(num):
+    #    def Transport_change_main():
+    #        print('Change to ' + str(num))
+    #        for i,name in enumerate(Transport_list):
+    #            Transport_Buttons[i] = tk.ACTIVE
+    #        Transport_Buttons[num]=tk.DISABLED
+    #        third_frame_manual_frames[num].tkraise()
+    #    return Transport_change_main
+    #for i,name in enumerate(Transport_list):
+    #    Transport_Buttons.append(ttk.Button(second_frame_manual,text=name,command=Transport_change(i)))
+    #    Transport_Buttons[i].pack(side='left',expand=1,fill='both')
+        
+
 
 
     
@@ -106,12 +111,23 @@ def main_edit(widget):
         third_frame_manual_frames[i].grid(row=0,column=0,sticky='nsew')
         label = ttk.Label(third_frame_manual_frames[i],text=name)
         label.pack()
-    
 
         
-    Change_Button(config.Default_edit)
-    Transport_change(0)()
+    #Change_Button(config.Default_edit)
+    #Transport_change(0)()
 
+    button_event.ChangeModeButtons(
+        select_frame,
+        MODE_list,
+        active_frame_list = [[second_frame_auto,third_frame_auto],[second_frame_manual,third_frame_manual]]
+    )
+    
+    button_event.ChangeModeButtons(
+        second_frame_manual,
+        Transport_list,
+        active_frame = third_frame_manual_frames
+    )
+    
 
 #-----------------------------------Footer_frame---------------------------
     frame = ttk.Frame(widget)
--- GUI_TEMPLATE.py
+++ GUI_TEMPLATE.py
@@ -1,17 +1,39 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 
+class input:
+    def Clear():
+        print('Clear')
+
 class button_event:
-    def Sample():
-        print('Hello')
+    def ChangeModeButtons(self,list,active_frame=None,active_frame_list=None):
+        def Change_frame(num):
+            def Change_frame_main():
+                for i in Buttons:
+                    i.config(state=tk.ACTIVE)
+                Buttons[num].config(state=tk.DISABLED)
+                if active_frame != None:
+                    active_frame[num].tkraise()
+                
+                if active_frame_list != None:
+                    for i in active_frame_list[num]:
+                        i.tkraise()
+                print(num)
+            return Change_frame_main
+        Buttons = []
+        for i,name in enumerate(list):
+            Buttons.append(ttk.Button(self,text=name,command=Change_frame(i)))
+            Buttons[i].pack(side='left',expand=1,fill='both')
+
 
 
 
 class widget_template:
-    def reference_file(widget,text=''):
-        label = ttk.Label(widget,text='ファイル',width=10)
+    def reference_file(self,text=''):
+        label = ttk.Label(self,text='ファイル',width=10)
         label.pack(side=tk.LEFT)
-        entry = ttk.Entry(widget,text=text)
+        entry = ttk.Entry(self,text=text)
         entry.pack(side=tk.LEFT,expand=1,fill=tk.X)
-        button = ttk.Button(widget,text='参照',width=5)
+        button = ttk.Button(self,text='参照',width=5)
         button.pack(side=tk.LEFT)
+        
